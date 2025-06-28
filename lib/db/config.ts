c254fda54a8b45b3047512e4cdd6838fef271a5d
ibnuhabibr <ibnuhabib017@gmail.com>
2025-06-28 20:55:58 +0700

    initial commit

--- lib/db/config.ts
+++ lib/db/config.ts
@@ -1,30 +1,24 @@
-import mysql from 'mysql2/promise';
+// Lokasi: lib/db/config.ts
 
-let pool;
+import mysql from 'mysql2/promise';
+import type { Pool } from 'mysql2/promise';
+
+let pool: Pool;
 
 // Logika ini akan membaca DATABASE_URL jika dideploy ke Vercel nanti
 // Tapi saat di lokal, dia akan membaca variabel DB_* dari .env.local
 if (process.env.DATABASE_URL) {
-  // Konfigurasi untuk production (VPS/Hosting)
+  // BAGIAN INI UNTUK VPS (PRODUCTION)
   pool = mysql.createPool({
     uri: process.env.DATABASE_URL,
-    ssl: {
-        // Jangan tolak koneksi yang tidak terverifikasi (jika pakai self-signed certificate)
-        // Di production asli, sebaiknya gunakan sertifikat SSL yang valid
-        rejectUnauthorized: false 
-    }
+    // ...
   });
 } else {
-  // Konfigurasi untuk development lokal (XAMPP)
+  // BAGIAN INI UNTUK LOKAL (XAMPP)
   pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'kitversity',
-    port: parseInt(process.env.DB_PORT || '3306'),
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    // ...
   });
 }
 
